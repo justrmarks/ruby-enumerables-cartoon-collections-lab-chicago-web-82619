db985a40855056b52377b18e21070a511250b3ca
Justice Marks <her@shedaddy.com>
2019-07-24 22:06:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,10 +19,14 @@ def long_planeteer_calls(calls)
 end
 
 
-def find_the_cheese(food)# code an argument here
+def find_the_cheese(food)
+  # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
   result = FALSE
+  
+  
   
   food.
 end
