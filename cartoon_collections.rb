caed451a24d6914f540e3425b03128a575bfc0c8
Justice Marks <her@shedaddy.com>
2019-07-24 22:00:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.collect{|elements| "#{elements.capitalize}!"}
 end
 
-def long_planeteer_calls(calls)# code an argument here
+def long_planeteer_calls(calls)
+  # code an argument here
   # Your code here
   
   return calls.all?{|call| word.length >4}
